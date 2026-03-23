66a9c71f17bcdb5ecbbeeff6e7e8bf5f58dee872
Saffron <redsavage2x@gmail.com>
2026-03-23 15:49:11 -0400

    feat: added models, db, and setup fastAPI sheep

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 from fastapi import FastAPI
+from models.db import db
+from models.models import Sheep
 
 app = FastAPI()
+
+@app.get("/sheep/{sheep_id}", response_model=Sheep)
+def read_sheep(id: int):
+    return db.get_sheep(id)
+
